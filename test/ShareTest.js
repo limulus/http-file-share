79be67265de20bbf5ac38730b1172c531e623207
Eric McCarthy <eric@limulus.net>
2014-09-13 15:34:12 -0700

    Support for DELETE and correct status code on unsupported request methods.

--- test/ShareTest.js
+++ test/ShareTest.js
@@ -50,17 +50,31 @@ describe("Share", function () {
   describe(".prototype.handleRequest", function () {
     var response
 
-    var get = function (url) {
+    var createRequest = function (method, url) {
       return httpMocks.createRequest({
-        "method": "GET",
+        "method": method,
         "url": url
       })
     }
+
+    var get = function (url) { return createRequest("GET", url) }
+    var del = function (url) { return createRequest("DELETE", url) }
 
     beforeEach(function () {
       response = httpMocks.createResponse({
         "eventEmitter": EventEmitter
       })
+    })
+
+    it("should respond with a 501 for an unsupported request method", function (done) {
+      var request = createRequest("TRACE", "/blarggg")
+
+      response.on("end", function () {
+        assert.strictEqual(response.statusCode, 501)
+        return done()
+      })
+
+      share.handleRequest(request, response)
     })
 
     it("should respond with a 404 for a nonexistent file", function (done) {
@@ -93,6 +107,18 @@ describe("Share", function () {
         assert.ok(JSON.parse(fileContent))
         assert.ok(JSON.parse(response._getData()))
         assert.strictEqual(response._getData().toString(), fileContent.toString())
+        return done()
+      })
+
+      share.handleRequest(request, response)
+    })
+
+    it("should respond with a 204 when a file is successfully deleted" , function (done) {
+      var request = del("/a.json")
+
+      response.on("end", function () {
+        assert.strictEqual(response.statusCode, 204)
+        assert.ok(! fs.existsSync(tempDirPath + "/a.json"))
         return done()
       })
 
