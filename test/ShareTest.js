dd80fe106beab7f35af409c2e86815b4561e52e9
Eric McCarthy <eric@limulus.net>
2014-09-13 14:48:36 -0700

    Serve files from a directory, or return 404 when not found.

--- test/ShareTest.js
+++ test/ShareTest.js
@@ -4,6 +4,8 @@ var temp = require("temp").track()
 var ncp = require("ncp")
 var assert = require("assert")
 var fs = require("fs")
+var httpMocks = require("node-mocks-http")
+var EventEmitter = require("events").EventEmitter
 
 var Share = require("../lib/Share.js")
 
@@ -42,6 +44,59 @@ describe("Share", function () {
     fs.exists(tempDirPath + "/a.json", function (result) {
       assert.ok(result)
       return done()
+    })
+  })
+
+  describe(".prototype.handleRequest", function () {
+    var response
+
+    var get = function (url) {
+      return httpMocks.createRequest({
+        "method": "GET",
+        "url": url
+      })
+    }
+
+    beforeEach(function () {
+      response = httpMocks.createResponse({
+        "eventEmitter": EventEmitter
+      })
+    })
+
+    it("should respond with a 404 for a nonexistent file", function (done) {
+      var request = get("/does-not-exist.txt")
+
+      response.on("end", function () {
+        assert.strictEqual(response.statusCode, 404)
+        return done()
+      })
+
+      share.handleRequest(request, response)
+    })
+
+    it("should respond with a 200 for a file that exists", function (done) {
+      var request = get("/a.json")
+
+      response.on("end", function () {
+        assert.strictEqual(response.statusCode, 200)
+        return done()
+      })
+
+      share.handleRequest(request, response)
+    })
+
+    it("should respond with the contents of the requested file", function (done) {
+      var request = get("/a.json")
+
+      response.on("end", function () {
+        var fileContent = fs.readFileSync(tempDirPath + "/a.json")
+        assert.ok(JSON.parse(fileContent))
+        assert.ok(JSON.parse(response._getData()))
+        assert.strictEqual(response._getData().toString(), fileContent.toString())
+        return done()
+      })
+
+      share.handleRequest(request, response)
     })
   })
 })
