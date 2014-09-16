d5be912d7b22770eb834c71822b6d912cdcfb372
Eric McCarthy <eric@limulus.net>
2014-09-15 22:20:07 -0700

    Don't respond to requests that do not match our base route.

--- test/http-server-integration.js
+++ test/http-server-integration.js
@@ -14,7 +14,7 @@ describe("http-server-integration", function () {
   var server, port
 
   beforeEach(function (done) {
-    server = http.createServer(createFileShare(sharePath))
+    server = http.createServer(createFileShare(sharePath, /^\/foo/))
     portfinder.getPort(function (err, thePort) {
       assert.ifError(err)
 
@@ -33,11 +33,26 @@ describe("http-server-integration", function () {
 
   it("should be able to successfully get a file from the test server", function (done) {
     var fileContent = fs.readFileSync(path.join(sharePath, "/1/b.json")).toString()
-    request("http://127.0.0.1:"+port+"/1/b.json", function (err, res, body) {
+    request("http://127.0.0.1:"+port+"/foo/1/b.json", function (err, res, body) {
       assert.ifError(err)
       assert.strictEqual(res.statusCode, 200)
       assert.strictEqual(res.headers["content-type"], "application/json")
       assert.strictEqual(body, fileContent)
+      return done()
+    })
+  })
+
+  it("should not respond when requested URL does not match route", function (done) {
+    server.on("request", function (req, res) {
+      if (req.url.match(/^\/bar/)) {
+        res.writeHead(418)
+        res.end()
+      }
+    })
+
+    request("http://127.0.0.1:"+port+"/bar/a.json", function (err, res) {
+      assert.ifError(err)
+      assert.strictEqual(res.statusCode, 418)
       return done()
     })
   })
